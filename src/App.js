7655ea69896744bb443734f745a5a36eb53bd7ee
mostafamagdy908 <mostafamagdy908@yahoo.com>
2024-11-23 08:29:09 +0200

    Initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import  "./index.css"
+
+const App = () => {
+  return (
+    <div className="title">
+      <h1> hellow </h1>
+      <h2>mostafa koj</h2>
+    </div>
+  )
+}
+
+export default App;
